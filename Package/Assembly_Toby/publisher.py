75c99cfc5ad089d90e3aa584b8e03a09bd7b939a
katyakochurova <katyakochurova@bk.ru>
2024-05-15 22:10:15 +0300

    fix J

--- Package/Assembly_Toby/publisher.py
+++ Package/Assembly_Toby/publisher.py
@@ -110,8 +110,7 @@ def plotcircle():
     a36 = sym.diff(pz, theta6)
     a37 = sym.diff(pz, theta7)
 
-    J = sym.Matrix([[a11, a12, a13, a14, a15, a16, a17], [a21, a22, a23, a24, a25, a26, a27],[a31, a32, a33, a34, a35, a36, a37],[Z1, Z2, Z3, Z4, Z5, Z6, Z7]])
-
+    J = sym.Matrix([[a11, a12, a13, a14, a15, a16, a17], [a21, a22, a23, a24, a25, a26, a27],[a31, a32, a33, a34, a35, a36, a37],[Z1, Z2, Z3, Z4, Z5, Z6, Z7], [0, 0, 0, 0, 0, 0, 1]])
     # Calculate Potential Energy
     P1 = -1*m1*g*(O1[2]+O0[2])*0.5
     P2 = -1*(m1+m2)*g*(O2[2]+O1[2])*0.5
